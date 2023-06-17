74741b55d0f54940275d007cb9089eed8e326bfb
Lucie <desmons.lucie@gmail.com>
2023-06-17 15:31:59 +0200

    [feat] add IAddressService and AddressService

---
+++ BLL/Interfaces/IAddressService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DATA.DTO;
+
+namespace BLL.Services
+{
+    public interface IAddressService
+    {
+        AddressDto GetAddressById(int addressId);
+        List<AddressDto> GetAllAddresses();
+        void CreateAddress(AddressDto addressDto);
+        void UpdateAddress(AddressDto addressDto);
+        void DeleteAddress(int addressId);
+    }
+}
---
+++ BLL/Services/AddressService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using DATA.DAL.Entities;
+using DATA.DAL.Repositories;
+using DATA.DTO;
+
+namespace BLL.Services
+{
+    public class AddressService
+    {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressService(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public AddressDto GetAddressById(int addressId)
+        {
+            Address address = _addressRepository.GetAddressById(addressId);
+            return AddressMapper.MapToDto(address);
+        }
+
+        public List<AddressDto> GetAllAddresses()
+        {
+            List<Address> addresses = _addressRepository.GetAllAddresses();
+            return AddressMapper.MapToDtoList(addresses);
+        }
+
+        public AddressDto CreateAddress(AddressDto addressDto)
+        {
+            Address address = AddressMapper.MapToEntity(addressDto);
+            address = _addressRepository.CreateAddress(address);
+            return AddressMapper.MapToDto(address);
+        }
+
+        public AddressDto UpdateAddress(AddressDto addressDto)
+        {
+            Address existingAddress = _addressRepository.GetAddressById(addressDto.IdAddress);
+
+            if (existingAddress == null)
+            {
+                throw new Exception("Address not found");
+            }
+
+            Address updatedAddress = AddressMapper.MapToEntity(addressDto);
+            updatedAddress = _addressRepository.UpdateAddress(updatedAddress);
+            return AddressMapper.MapToDto(updatedAddress);
+        }
+
+        public void DeleteAddress(int addressId)
+        {
+            Address existingAddress = _addressRepository.GetAddressById(addressId);
+
+            if (existingAddress == null)
+            {
+                throw new Exception("Address not found");
+            }
+
+            _addressRepository.DeleteAddress(existingAddress);
+        }
+    }
+}
