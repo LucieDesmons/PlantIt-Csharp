135f62ef48608aa1da319f3f8cdabebe95a53181
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:44:44 +0200

    [feat] add PlantMapper

---
+++ BLL/Mappers/PlantMapper.cs
@@ -0,0 +1,46 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class PlantMapper
+    {
+        public static PlantDto MapToDto(Plant plant)
+        {
+            return new PlantDto
+            {
+                IdPlant = plant.IdPlant,
+                Name = plant.Name,
+                PlacePlant = plant.PlacePlant,
+                Container = plant.Container,
+                Humidity = plant.Humidity,
+                Clarity = plant.Clarity,
+                IdUser = plant.IdUser,
+                IdPlantReference = plant.IdPlantReference,
+                PlantReference = PlantReferenceMapper.MapToDto(plant.PlantReference),
+                User = UserMapper.MapToDto(plant.User),
+                ConversationCollection = plant.ConversationCollection.Select(ConversationMapper.MapToDto).ToList(),
+                PictureCollection = plant.PictureCollection.Select(PictureMapper.MapToDto).ToList()
+            };
+        }
+
+        public static Plant MapToEntity(PlantDto plantDto)
+        {
+            return new Plant
+            {
+                IdPlant = plantDto.IdPlant,
+                Name = plantDto.Name,
+                PlacePlant = plantDto.PlacePlant,
+                Container = plantDto.Container,
+                Humidity = plantDto.Humidity,
+                Clarity = plantDto.Clarity,
+                IdUser = plantDto.IdUser,
+                IdPlantReference = plantDto.IdPlantReference,
+                PlantReference = PlantReferenceMapper.MapToEntity(plantDto.PlantReference),
+                User = UserMapper.MapToEntity(plantDto.User),
+                ConversationCollection = plantDto.ConversationCollection.Select(ConversationMapper.MapToEntity).ToList(),
+                PictureCollection = plantDto.PictureCollection.Select(PictureMapper.MapToEntity).ToList()
+            };
+        }
+    }
+}
