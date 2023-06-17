bcb3f7746e684d446644d0e84fb12bd6e2b23908
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:43:44 +0200

    [feat] add PasswordHistoricMapper

---
+++ BLL/Mappers/PasswordHistoricMapper.cs
@@ -0,0 +1,32 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class PasswordHistoricMapper
+    {
+        public static PasswordHistoricDto MapToDto(PasswordHistoric passwordHistoric)
+        {
+            return new PasswordHistoricDto
+            {
+                IdHistoric = passwordHistoric.IdHistoric,
+                Password = passwordHistoric.Password,
+                UpdateDate = passwordHistoric.UpdateDate,
+                IdAuthentification = passwordHistoric.IdAuthentification,
+                Authentification = AuthentificationMapper.MapToDto(passwordHistoric.Authentification)
+            };
+        }
+
+        public static PasswordHistoric MapToEntity(PasswordHistoricDto passwordHistoricDto)
+        {
+            return new PasswordHistoric
+            {
+                IdHistoric = passwordHistoricDto.IdHistoric,
+                Password = passwordHistoricDto.Password,
+                UpdateDate = passwordHistoricDto.UpdateDate,
+                IdAuthentification = passwordHistoricDto.IdAuthentification,
+                Authentification = AuthentificationMapper.MapToEntity(passwordHistoricDto.Authentification)
+            };
+        }
+    }
+}
