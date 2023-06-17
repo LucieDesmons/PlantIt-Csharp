fd33ef1a07970dc72509b0a7ce55f2bedcfb1d17
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:42:05 +0200

    [feat] add AuthentificationMapper

---
+++ BLL/Mappers/AuthentificationMapper.cs
@@ -0,0 +1,32 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class AuthentificationMapper
+    {
+        public static AuthentificationDto MapToDto(Authentification authentification)
+        {
+            return new AuthentificationDto
+            {
+                IdAuthentification = authentification.IdAuthentification,
+                Email = authentification.Email,
+                Password = authentification.Password,
+                User = UserMapper.MapToDto(authentification.User),
+                PasswordHistoricCollection = authentification.PasswordHistoricCollection.Select(PasswordHistoricMapper.MapToDto).ToList()
+            };
+        }
+
+        public static Authentification MapToEntity(AuthentificationDto authentificationDto)
+        {
+            return new Authentification
+            {
+                IdAuthentification = authentificationDto.IdAuthentification,
+                Email = authentificationDto.Email,
+                Password = authentificationDto.Password,
+                User = UserMapper.MapToEntity(authentificationDto.User),
+                PasswordHistoricCollection = authentificationDto.PasswordHistoricCollection.Select(PasswordHistoricMapper.MapToEntity).ToList()
+            };
+        }
+    }
+}
