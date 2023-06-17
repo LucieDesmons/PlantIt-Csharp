96bb31a2c66faabd9ae98f921f7ba6309d235d64
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:45:56 +0200

    [feat] add UserTypeMapper

---
+++ BLL/Mappers/UserTypeMapper.cs
@@ -0,0 +1,28 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class UserTypeMapper
+    {
+        public static UserTypeDto MapToDto(UserType userType)
+        {
+            return new UserTypeDto
+            {
+                IdUserType = userType.IdUserType,
+                Label = userType.Label,
+                UserCollection = userType.UserCollection.Select(UserMapper.MapToDto).ToList()
+            };
+        }
+
+        public static UserType MapToEntity(UserTypeDto userTypeDto)
+        {
+            return new UserType
+            {
+                IdUserType = userTypeDto.IdUserType,
+                Label = userTypeDto.Label,
+                UserCollection = userTypeDto.UserCollection.Select(UserMapper.MapToEntity).ToList()
+            };
+        }
+    }
+}
