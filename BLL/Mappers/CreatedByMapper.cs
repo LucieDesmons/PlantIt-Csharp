81605520b8a98c0580f81c1e1c8cabb8a32a9a0f
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:42:55 +0200

    [feat] add CreatedByMapper

---
+++ BLL/Mappers/CreatedByMapper.cs
@@ -0,0 +1,34 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class CreatedByMapper
+    {
+        public static CreatedByDto MapToDto(CreatedBy createdBy)
+        {
+            return new CreatedByDto
+            {
+                IdPlantReference = createdBy.IdPlantReference,
+                IdUser = createdBy.IdUser,
+                OrderNum = createdBy.OrderNum,
+                UpdateDate = createdBy.UpdateDate,
+                PlantReference = PlantReferenceMapper.MapToDto(createdBy.PlantReference),
+                User = UserMapper.MapToDto(createdBy.User)
+            };
+        }
+
+        public static CreatedBy MapToEntity(CreatedByDto createdByDto)
+        {
+            return new CreatedBy
+            {
+                IdPlantReference = createdByDto.IdPlantReference,
+                IdUser = createdByDto.IdUser,
+                OrderNum = createdByDto.OrderNum,
+                UpdateDate = createdByDto.UpdateDate,
+                PlantReference = PlantReferenceMapper.MapToEntity(createdByDto.PlantReference),
+                User = UserMapper.MapToEntity(createdByDto.User)
+            };
+        }
+    }
+}
