0d3f4640a6c2e7d478b2afbee7fd4a6d7602cc04
Lucie <desmons.lucie@gmail.com>
2023-06-17 14:44:08 +0200

    [feat] add PictureReferenceMapper

---
+++ BLL/Mappers/PictureReferenceMapper.cs
@@ -0,0 +1,30 @@
+﻿using DATA.DAL.Entities;
+using DATA.DTO;
+
+namespace BLL.Mappers
+{
+    public static class PictureReferenceMapper
+    {
+        public static PictureReferenceDto MapToDto(PictureReference pictureReference)
+        {
+            return new PictureReferenceDto
+            {
+                IdPictureReference = pictureReference.IdPictureReference,
+                Path = pictureReference.Path,
+                ModificationDate = pictureReference.ModificationDate,
+                PlantReferenceCollection = pictureReference.PlantReferenceCollection.Select(PlantReferenceMapper.MapToDto).ToList()
+            };
+        }
+
+        public static PictureReference MapToEntity(PictureReferenceDto pictureReferenceDto)
+        {
+            return new PictureReference
+            {
+                IdPictureReference = pictureReferenceDto.IdPictureReference,
+                Path = pictureReferenceDto.Path,
+                ModificationDate = pictureReferenceDto.ModificationDate,
+                PlantReferenceCollection = pictureReferenceDto.PlantReferenceCollection.Select(PlantReferenceMapper.MapToEntity).ToList()
+            };
+        }
+    }
+}
